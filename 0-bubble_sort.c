2ef9e866b5c73182333d1bdd0f555fb2de9306c4
Esperanza211 <khadijazaabar05@gmail.com>
2023-11-18 05:42:54 -0800

    sorting alg

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,38 +1,35 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sort array elements from min to max value
- * @array: array
+ * bubble_sort - sorts an array of integers in ascending order
  * using the Bubble sort algorithm.
- * @size: array size
+ * @array: the array to sort
+ * @size: size of the array
  * Return: (void)
- */
+*/
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, index;
-    int tmp;
-    int swapped;
+	size_t i, j;
+	int swaped;
 
-    if (size < 2 || array == NULL)
-        return;
+	if (!array)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-
-        for (index = 0; index < size - 1 - i; index++)
-        {
-            if (array[index] > array[index + 1])
-            {
-                tmp = array[index];
-                array[index] = array[index + 1];
-                array[index + 1] = tmp;
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
-
-        if (!swapped)
-            break;
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		swaped = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				array[j] = array[j] + array[j + 1];
+				array[j + 1] = array[j] - array[j + 1];
+				array[j] = array[j] - array[j + 1];
+				swaped = 1;
+				print_array(array, size);
+			}
+		}
+		if (!swaped)
+			break;
+	}
 }
