9905b052d5e5b2c93e1a42ec380e98330ab05a0e
Esperanza211 <khadijazaabar05@gmail.com>
2023-11-20 07:21:52 -0800

    shell sort

--- sort.h
+++ sort.h
@@ -31,6 +31,6 @@ int lomuto_partition(int *array, int start, int end, size_t size);
 void perform_swap(int *a, int *b);
 void print_array_after_swap(int *array, size_t size);
 
-
+void shell_sort(int *array, size_t size);
 
 #endif
