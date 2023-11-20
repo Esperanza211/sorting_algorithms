1264c9fb027d4ea5bf27903ad329c3525c452893
imanebenhadda <imanebenhaddapama@gmail.com>
2023-11-20 13:39:34 -0800

    cocktail

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -2,14 +2,25 @@
 #include <stdio.h>
 
 /**
- * swap_node - swap a node with its previous one
- * @node: node to be swapped
- * @list: pointer to the head of the list
- * Return: pointer to the node that was swapped
+ * swap_node - Swap a node with its previous one in a doubly linked list
+ * @node: Node to be swapped
+ * @list: Pointer to the head of the list
+ * Return: Pointer to the node that was swapped
  */
 listint_t *swap_node(listint_t *node, listint_t **list)
 {
 	listint_t *back = node->prev, *current = node;
+
+	if (!back)
+	{
+		*list = current;
+		current->prev = NULL;
+		current->next = back->next;
+		if (back->next)
+			back->next->prev = current;
+		back->next = back;
+		return (current);
+	}
 
 	back->next = current->next;
 	if (current->next)
@@ -27,20 +38,19 @@ listint_t *swap_node(listint_t *node, listint_t **list)
 
 /**
  * cocktail_sort_list - Cocktail sort implementation for a doubly linked list
- * @list: pointer to the head of the list
+ * @list: Pointer to the head of the list
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *node;
+	listint_t *node = *list;
 	int swapped;
 
 	if (!list || !(*list) || !(*list)->next)
 		return;
 
-	node = *list;
 	do {
 		swapped = 0;
-		while (node->next)
+		while (node && node->next)
 		{
 			if (node->n > node->next->n)
 			{
@@ -56,7 +66,7 @@ void cocktail_sort_list(listint_t **list)
 			break;
 
 		swapped = 0;
-		while (node->prev)
+		while (node && node->prev)
 		{
 			if (node->n < node->prev->n)
 			{
