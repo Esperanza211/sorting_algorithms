7a59f6d41d593b594acef3a8e8feb42c61071cc2
Esperanza211 <khadijazaabar05@gmail.com>
2023-11-20 07:24:21 -0800

    shell

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,37 +1,38 @@
-#include "sort.h"
+#include	"sort.h"
 
 /**
- * shell_sort - Sorts an array of integers in ascending order using Shell Sort.
- * @array: The array to be sorted.
- * @size: The size of the array.
- */
-void shell_sort(int *array, size_t size)
+	*	shell_sort	-	Sorts	an	array	of	integers	in	ascending	order	using	Shell	Sort.
+	*	@array:	The	array	to	be	sorted.
+	*	@size:	The	size	of	the	array.
+	*/
+void	shell_sort(int	*array,	size_t	size)
 {
-	int gap, i;
-	if (!array || size < 2)
-		return;
+	int	gap,	i;
 
-	gap = 1;
+	if	(!array	||	size	<	2)
+	return;
 
-	while (gap < (int)size / 3)
-		gap = gap * 3 + 1;
+	gap	=	1;
 
-	while (gap > 0)
+	while	(gap	<	(int)size	/	3)
+	gap	=	gap	*	3	+	1;
+
+	while	(gap	>	0)
 	{
-		for (i = gap; i < (int)size; i++)
-		{
-			int temp = array[i];
-			int j;
+	for	(i	=	gap;	i	<	(int)size;	i++)
+	{
+	int	temp	=	array[i];
+	int	j;
 
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-			{
-				array[j] = array[j - gap];
-			}
+	for	(j	=	i;	j	>=	gap	&&	array[j	-	gap]	>	temp;	j	-=	gap)
+	{
+	array[j]	=	array[j	-	gap];
+	}
 
-			array[j] = temp;
-		}
+	array[j]	=	temp;
+	}
 
-		print_array(array, size);
-		gap = (gap - 1) / 3;
+	print_array(array,	size);
+	gap	=	(gap	-	1)	/	3;
 	}
 }
